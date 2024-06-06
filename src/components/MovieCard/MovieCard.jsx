242122e3ac6ee18c0aea3c9bdb7a666a982bd07b
Yi-HyeonJu <guswwn1925@gmail.com>
2024-06-06 21:38:47 +0900

    mission3

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -6,26 +6,28 @@ function MovieCard({ movieData }) {
 
   return (
       <section className="card_container">
-          {movieData.map((movie) => (
-            <div
-              className='card_movie-container'
-              key={movie.id}
+          {movieData.map((movie) => {
+            if(movie.backdrop_path || movie.poster_path !== undefined){
+              <div
+                className='card_movie-container'
+                key={movie.id}
               >
-              <Link to={`/movie/${movie.id}`} style={{ textDecoration: "none"}} >
-                  <img
-                  className='card_movie-img'
-                  alt="영화 이미지"
-                  src={`http://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
-                  />
-                  <p className='card_movie-title'>
-                    {movie.title}
-                  </p>
-              </Link>
+                <Link to={`/movie/${movie.id}`} style={{ textDecoration: "none"}} >
+                    <img
+                      className='card_movie-img'
+                      alt={movie.title}
+                      src={`http://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                    />
+                    <p className='card_movie-title'>
+                      {movie.title}
+                    </p>
+                </Link>
               <p className='card_movie-vote_average'>
                 {movie.vote_average}
               </p>
             </div>
-          ))}
+          }
+        })}
       </section>
   );
 }
