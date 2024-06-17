8f5c322b344658fe5aec4095885b0e4528f9073a
Yi-HyeonJu <guswwn1925@gmail.com>
2024-06-17 23:39:02 +0900

    feat : swiper 수정 중

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import tmdbAPI from '../../api/tmdbAPI';
-import './Banner.css';
-import Swiper from 'swiper';
-import 'swiper/css';
+import React, { useEffect, useState } from "react";
+import tmdbAPI from "../../api/tmdbAPI";
+import "./Banner.css";
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
 
 function Banner() {
   const [upMovieData, setUpMovieData] = useState([]);
 
   const fetchUpMovieData = async () => {
     try {
-      const response = await tmdbAPI.get('movie/upcoming');
+      const response = await tmdbAPI.get("movie/upcoming");
       setUpMovieData(response.data.results);
       console.log(response.data.results);
     } catch (error) {
@@ -23,13 +24,24 @@ function Banner() {
 
   useEffect(() => {
     if (upMovieData.length > 0) {
-      const swiper = new Swiper('.mySwiper', {
+      const swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4, // 한 번에 3개의 슬라이드 표시
+        spaceBetween: 20, // 슬라이드 간의 간격
+        centeredSlides: true, // 중앙에 슬라이드 위치
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         },
         pagination: {
-          el: '.swiper-pagination',
+          el: ".swiper-pagination",
+          clickable: true,
         },
       });
 
@@ -40,20 +52,22 @@ function Banner() {
   }, [upMovieData]);
 
   return (
-    <section className='Banner_container swiper mySwiper'>
-      <div className='swiper-wrapper'>
+    <section className="Banner_container swiper mySwiper">
+      <div className="swiper-wrapper">
         {upMovieData.map((movie) => (
-          <div key={movie.id} className='swiper-slide'>
-            <img
-              src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}
-            />
+          <div key={movie.id} className="swiper-slide">
+            <div className="slide-content">
+              <img
+                src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
           </div>
         ))}
       </div>
-      <div className='swiper-pagination'></div>
-      <div className='swiper-button-prev'>{'<'}</div>
-      <div className='swiper-button-next'>{'>'}</div>
+      <div className="swiper-pagination"></div>
+      <div className="swiper-button-prev">{"<"}</div>
+      <div className="swiper-button-next">{">"}</div>
     </section>
   );
 }
